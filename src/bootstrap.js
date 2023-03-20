30a33425f449064b69e80476c3253a451fcdd6ce
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-03-20 13:47:51 -0500

    consuming remote modules

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,6 +1,5 @@
 import faker from 'faker';
 
 const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
-console.log(cardText);
 
 document.querySelector('#dev-cart').innerHTML = cartText;
